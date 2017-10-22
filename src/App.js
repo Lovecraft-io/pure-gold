5bde297a4460014c47eabf790067664256860939
Liam Ellis <liamhellis@gmail.com>
2017-10-22 18:14:18 -0500

    css

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ import About from './Components/About'
 import Contact from './Components/Contact'
 import Header from './Components/Partials/Header'
 import Footer from './Components/Partials/Footer'
-
 import './App.css';
-
-const Vimeo = require('vimeo').Vimeo;
-const clientID = 'a403a67f112f44d9826223d299688cffcdc4c794';
-const clientSecret = '4ddwI9d7nHrcRcPlM5UE4YyNTZ9yjCCthnz+9CwY4Hdg5JkaYrOWRu24kgmnq6qmkDXNZ2EejDZ6ZmgmGM6pmMX+m02NeAPx4fty7Dc5rMCwIT1iZ/+T5Goz0xx4aoFV';
-const accessToken = '827ba89c1ca4647189da72d793d669b5';
-
-
-const isPublic = (video) => video.privacy.view != 'nobody' && video.privacy.view != 'password'
-
 
 class App extends Component {
   constructor(props) {
@@ -29,30 +19,6 @@ class App extends Component {
   }
   componentWillMount() {
 
-    if (localStorage.getItem('videos')){
-      let videos = JSON.parse(localStorage.getItem('videos'))
-      videos = videos.filter(video => isPublic(video))
-      console.log(videos)
-      this.setState({ videos: videos })
-
-    } else {
-      const lib = new Vimeo(clientID, clientSecret, accessToken);
-      let videos = [];
-      
-      lib.request({
-          path: '/me/videos'
-      }, (err, body, status_code, headers) => {
-          if (!err) {
-              videos = body.data.filter((video) => isPublic(video))
-              localStorage.setItem('videos', JSON.stringify(videos))
-              console.log(localStorage.getItem('videos'))
-              this.setState({ videos: videos })
-          } else {
-              console.log(err);
-          }
-  
-      })
-    }
   }
   componentDidMount() {
 
@@ -66,7 +32,7 @@ class App extends Component {
           <Header />
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
-          <Route exact path='/portfolio'  render={() => <Portfolio videos={this.state.videos}/> }/>
+          <Route exact path='/portfolio'  component={Portfolio}/>
           <Route exact path='/contact' component={Contact} />
         </div>
       </BrowserRouter>
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
-import Vimeo from 'react-vimeo'
-
-
+// import Vimeo from 'react-vimeo'
 import '../css/Portfolio.css'
+// import {Route, Link} from 'react-router-dom'
+// import MobilePortfolio from './MobilePortfolio'
+// <Route exact path='/mobile' component={MobilePortfolio} videos={this.state.videos}/>
+// 				<Link to='/mobile'>MObile</Link>
+
+const Vimeo = require('vimeo').Vimeo;
+const clientID = 'a403a67f112f44d9826223d299688cffcdc4c794'
+const clientSecret = '4ddwI9d7nHrcRcPlM5UE4YyNTZ9yjCCthnz+9CwY4Hdg5JkaYrOWRu24kgmnq6qmkDXNZ2EejDZ6ZmgmGM6pmMX+m02NeAPx4fty7Dc5rMCwIT1iZ/+T5Goz0xx4aoFV'
+const accessToken = '827ba89c1ca4647189da72d793d669b5'
+const isPublic = (video) => video.privacy.view != 'nobody' && video.privacy.view != 'password'
+
+
+
 
 
 export default class Portfolio extends Component {
@@ -14,6 +25,32 @@ export default class Portfolio extends Component {
 		}
 
 
+	}
+	componentWillMount(){
+		if (localStorage.getItem('videos')){
+			let videos = JSON.parse(localStorage.getItem('videos'))
+			videos = videos.filter(video => isPublic(video))
+			console.log(videos)
+			this.setState({ videos: videos })
+	  
+		  } else {
+			const lib = new Vimeo(clientID, clientSecret, accessToken);
+			let videos = [];
+			
+			lib.request({
+				path: '/me/videos'
+			}, (err, body, status_code, headers) => {
+				if (!err) {
+					videos = body.data.filter((video) => isPublic(video))
+					localStorage.setItem('videos', JSON.stringify(videos))
+					console.log(localStorage.getItem('videos'))
+					this.setState({ videos: videos })
+				} else {
+					console.log(err);
+				}
+		
+			})
+		  }
 	}
 
 
@@ -31,9 +68,14 @@ export default class Portfolio extends Component {
 			display: 'block',
 			margin: 'auto'
 		}
-		const mobile_videos = this.props.videos.slice(0, 4)
-		const videos = this.props.videos.map(video => {
+		
+		const videos = this.state.videos.map(video => {
 			let bg_image
+			let date = new Date(video.release_time)
+			let day = date.getDay()
+			let month = date.getMonth()
+			let year = date.getFullYear()
+			let video_date = `${day}/${month}/${year}`
 			if (video.pictures){
 				bg_image =  video.pictures.sizes[5].link
 				let bg_style = {
@@ -43,19 +85,17 @@ export default class Portfolio extends Component {
 						<div className="video__section">
 							<img src={bg_image} alt="" className="video__section_bg" />
 							<div className="video__wrap active">
+								<div className="content__slide">
+									<h2 className="title"> <span className="title__line"> <span className="title__inner"></span></span><span className="title__line"> <span className="title__inner">{video.name}</span></span></h2>
+									<p className="desc">{video.description}</p>
+											
+								</div>
+								<div className="content__ping content__ping--noanimation"></div>
 								<div className="video__wrap_inner">
 									<div className="video__player" dangerouslySetInnerHTML={{__html: video.embed.html}}/>
-									<span className="number">01</span>
-									<span className="date">30 MARCH 2017 <span className="date__time">12:37PM</span></span>
-									<div className="content">
+									<span className="number">{video.stats.plays}</span>
+									<span className="date">{video_date}<span className="date__time"></span></span>
 									
-										<div className="content__slide">
-											<h2 className="title"> <span className="title__line"> <span className="title__inner">Breathtaking</span></span><span className="title__line"> <span className="title__inner">Heights</span></span></h2>
-											<p className="desc">Nunc orci metus, ornare non molestie ac, ultrices eget  <br/> dolor. Mauris ac mattis lectus. Praesent facilisis  <br/> malesuada sapien nec pharetra. Fusce eleifend, nisl.</p>
-											<div className="button-wrap"><a className="button">Learn More<span className="button__hover"></span></a></div>
-										</div>
-										<div className="content__ping content__ping--noanimation"></div>
-									</div>
 								</div>
 							</div>
 						</div>
@@ -67,6 +107,7 @@ export default class Portfolio extends Component {
 		return (
 			<div id="Portfolio">
 				{videos}
+				
 			</div>
 		)
 	}
